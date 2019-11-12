699bfb46c70eab895c0702406253260a1dc59a11
bherbruck <brennen.herbruck@gmail.com>
2019-11-12 03:10:22 -0500

    refactore

--- vid2img.py
+++ vid2img.py
@@ -1,35 +1,32 @@
-from os import path, mkdir
-from time import sleep
-from cv2 import VideoCapture, imwrite
+import os
+import time
+import cv2
 
-def capture_images(vidfile, interval=1.0):
+def capture_images(vidfile, interval=0):
     """Read a video file and convert to images
     
     Args:
         vidfile (str): video file to read
         interval (float, optional): how often to capture an image from the video in seconds. Defaults to 1.0.
     """
-    vidcap = VideoCapture(vidfile)
+    vidcap = cv2.VideoCapture(vidfile)
     success, image = vidcap.read()
     print(success)
     if success:
-        vidname = path.splitext(path.basename(vidfile))[0]
-        vidpath = path.dirname(vidfile)
+        vidname = os.path.splitext(os.path.basename(vidfile))[0]
+        vidpath = os.path.dirname(vidfile)
         imgpath = "{}/{}_images".format(vidpath, vidname)
         print("Found video at: ".format(vidpath))
         count = 0
-        if not path.exists(imgpath):
-            mkdir(imgpath)
+        if not os.path.exists(imgpath):
+            os.mkdir(imgpath)
             print("Creating directory: {}".format(imgpath))
         while success:
             success, image = vidcap.read()
             imgfile = "{}/{}_{}.jpg".format(imgpath, count, vidname)
-            imwrite(imgfile, image)
+            cv2.imwrite(imgfile, image)
             print("Creating image file: {}".format(imgfile))
             count += 1
-            sleep(interval)
+            time.sleep(interval)
     else:
         print("Something went wrong... check your file path/name")
-            
-
-capture_images("/home/brenn/Videos/eggvid.mp4")
